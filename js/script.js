fba9fa5d9c92c9b0b65a91b16157ac08899154ce
IvanGame <46857116+IvanGame@users.noreply.github.com>
2022-03-16 12:29:36 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        whaitForAnimate: false,
+        centerMode: true,
+    });
+});
