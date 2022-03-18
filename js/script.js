ce0ba4ad3b550cc3fcdf0a185ad31c630f43244b
IvanGame <46857116+IvanGame@users.noreply.github.com>
2022-03-18 15:01:57 +0700

    Support for mobile

--- js/script.js
+++ js/script.js
@@ -6,5 +6,33 @@ $(document).ready(function () {
         whaitForAnimate: false,
         centerMode: true,
         variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: false,
+                    variableWidth: false,
+                }
+            }
+        ]
     });
 });
+
+let header__burger = document.querySelector('.header__burger');
+let header__button = document.querySelector('.header__button');
+let header_menu = document.querySelector('.menu');
+let back = document.querySelector('body');
+let header__list = document.querySelector('.header__list');
+
+header__burger.onclick = function () {
+    header__burger.classList.toggle('active');
+    header__button.classList.toggle('active');
+    header_menu.classList.toggle('active');
+    back.classList.toggle('lock');
+}
+
+header__list.onclick = function () {
+    header__list.classList.remove('active');
+    back.classList.toggle('lock');
+}
